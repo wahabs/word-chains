6ef683f0e0e90e09d6ced1fe23c9c54d3947f7a6
Wahab Sheikh <wahabmsheikh@gmail.com>
2014-12-14 20:48:10 -0800

    Add ability to search and print cascading adjacent words

---
+++ word_chains.rb
@@ -0,0 +1,55 @@
+require 'Set'
+
+class WordChainer
+
+  attr_accessor :dictionary, :current_words, :all_seen_words
+
+  def initialize(dictionary_file_name = "dictionary.txt")
+    @dictionary = File.readlines(dictionary_file_name).map {|line| line.chomp}.to_set
+  end
+
+  def adjacent_words(word)
+    dictionary.select { |dict_word| is_adjacent?(dict_word, word)}
+  end
+
+  def run(source, target)
+    self.current_words = [source]
+    self.all_seen_words = [source]
+    until current_words.empty?
+      new_current_words = explore_current_words
+      #p new_current_words
+      self.current_words = new_current_words
+    end
+  end
+
+
+
+
+  private
+
+    def is_adjacent?(str1, str2)
+      return false if str1.length != str2.length
+      diff = 0
+      str1.split("").each_with_index do |char, i|
+        diff += 1 if char != str2[i]
+      end
+      diff == 1
+    end
+
+    def explore_current_words
+      new_current_words = []
+      current_words.each do |current_word|
+        adjacent_words(current_word).each do |adjacent_word|
+          unless all_seen_words.include?(adjacent_word)
+            new_current_words << adjacent_word
+            self.all_seen_words << adjacent_word
+          end
+        end
+      end
+      new_current_words
+    end
+
+end
+
+test = WordChainer.new
+test.run("cat", "far")
