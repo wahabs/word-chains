c131a97a10d5c8ad33dc3b0bf64b0b4326dd3b7d
Wahab Sheikh <wahabmsheikh@gmail.com>
2014-12-14 21:18:30 -0800

    Add in the ability to build a word chain

--- word_chains.rb
+++ word_chains.rb
@@ -14,15 +14,23 @@ class WordChainer
 
   def run(source, target)
     self.current_words = [source]
-    self.all_seen_words = [source]
-    until current_words.empty?
+    self.all_seen_words = { source => nil }
+    until current_words.empty? || all_seen_words.include?(target)
       new_current_words = explore_current_words
-      #p new_current_words
       self.current_words = new_current_words
     end
+    build_path(target)
   end
 
-
+  def build_path(target)
+    path = [target]
+    link = all_seen_words[target]
+    until link.nil?
+      path << link
+      link = all_seen_words[link]
+    end
+    path.reverse
+  end
 
 
   private
@@ -42,7 +50,7 @@ class WordChainer
         adjacent_words(current_word).each do |adjacent_word|
           unless all_seen_words.include?(adjacent_word)
             new_current_words << adjacent_word
-            self.all_seen_words << adjacent_word
+            self.all_seen_words[adjacent_word] = current_word
           end
         end
       end
@@ -51,5 +59,7 @@ class WordChainer
 
 end
 
-test = WordChainer.new
-test.run("cat", "far")
+
+if __FILE__ == $PROGRAM_NAME
+  p WordChainer.new(ARGV.shift).run("duck", "ruby")
+end
